7ee46f2de98265749a245596142e6b9b1c1225ec
srinu217 <gokarla_fw11_270@masai.school>
2021-11-08 16:24:41 +0530

    7th

---
+++ tailwand/foodapp/components/navbar.js
@@ -0,0 +1,11 @@
+function navbar() {
+    return `
+    <h3><a href="/">Home</a></h3>
+    <div id="options">
+        <h3><a href="food.html">Food App</a></h3>
+        <h3><a href="#">Sign In</a></h3>
+        <h3><a href="#">Sign Up</a></h3>
+        
+    </div>`
+    
+}
