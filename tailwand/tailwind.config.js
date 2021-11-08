322169781b7b1bdc69b11838a6604145a7e9024e
srinu217 <gokarla_fw11_270@masai.school>
2021-11-08 15:42:30 +0530

    5th

---
+++ tailwand/tailwind.config.js
@@ -0,0 +1,15 @@
+
+
+module.exports = {
+    theme: {
+        colors: {
+            srinivas: "#b1b1b1",
+            aabbcc: "black"
+        },
+    },
+    extend: {
+        spacing: {
+            '50': "100px"
+        },
+    },
+};
